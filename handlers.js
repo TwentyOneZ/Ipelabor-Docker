39fe8c8357a91806cf2f49ac480188534baa96e7
Gustavo Souza <TwentyOneZ@users.noreply.github.com>
2025-08-21 05:01:45 -0300

    Fix 04

--- handlers.js
+++ handlers.js
@@ -404,6 +404,7 @@ async function handleIncomingMessages(upsert, sock) {
     const msgId  = msg.key.id;
     const chatId = msg.key.remoteJid;
     const branch = getBranchByChatId(chatId);
+    logger.debug(`🔍 Debug: Mensagem do chatId: ${chatId}, detectado branch: ${branch}`);
     const topics = branch ? getTopicsByBranch(branch) : null;
     const text   = msg.message?.conversation
                 || msg.message?.extendedTextMessage?.text
